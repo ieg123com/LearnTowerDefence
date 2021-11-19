e69704f2e4edddf514cdfa58bdbc7f003af96cea
Ieg123 <154439441@qq.com>
2021-11-19 09:14:52 +0800

    加入攻击功能

---
+++ Assets/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+
+    public float speed = 70f;
+    public GameObject bulletImpactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+        if (dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    void HitTarget()
+    {
+        Debug.Log("WE HIT SOMETHING!");
+
+        GameObject effectIns = (GameObject)Instantiate(bulletImpactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2.0f);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/BulletManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    static public BulletManager Instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instance = this;
+
+    }
+
+    public GameObject standerTurretPrefab;
+
+    private void Start()
+    {
+        turretToBuild = standerTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+}
